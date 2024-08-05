f9a0aa33d451e22bf24406fc5c272badaaa01de4
enqti <yxcom7505@gmail.com>
2024-08-05 22:34:42 +0330

    Add files via upload

--- customing.java
+++ customing.java
@@ -1,8 +1,15 @@
 package net.enqti.customing;
 
+import net.enqti.customing.init.ItemInit;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 public class customing {
     @Mod("customing")
     public static final String MODID = "customing";
+     public customing() {
+         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+         ItemInit.ITEMS.register(bus);
+     }
 }
