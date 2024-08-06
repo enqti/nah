2ad32d6766ed0e4d074e58f78afbd3ff7c56d230
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-08-06 07:27:05 -0400

    Update ItemInit.java

--- ItemInit.java
+++ ItemInit.java
@@ -7,8 +7,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ItemInit {
-
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, customing.MODID);
 
-    public static final RegistryObject<Item> reinforced_iron = ITEMS.register("reinforced_iron", () -> new Item(new Item.Properties().stacksTo(64)));
+    public static final RegistryObject<Item> reinforced_iron = ITEMS.register("reinforced_iron", () -> new Item(new Item.Properties()));
 }
